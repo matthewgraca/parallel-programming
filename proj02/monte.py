6f3aa6b9f89a671ba7e50c93d0d8fffdcbf1e6a4
matthewgraca <mgraca@cpp.edu>
2022-11-02 13:13:47 -0700

    added input validation, completed assignment

--- proj02/monte.py
+++ proj02/monte.py
@@ -2,6 +2,8 @@ import threading
 import random
 import time
 import math
+import sys
+import getopt
  
 mutex = threading.Lock()
 globalCount = 0
@@ -24,11 +26,27 @@ def countInRange(partSize):
     global globalCount
     globalCount += localCount
     mutex.release()
- 
+
+def validateInputs(argv):
+    usage = "Usage: python monte.py [1...] [10...1000000]"
+    # ensure num of args is 2
+    if len(argv) != 2:
+        sys.exit("Two arguments required. " + usage)
+    # ensure two args are ints
+    try:
+        threads = int(argv[0])
+        points = int(argv[1])
+    except:
+        sys.exit("Arguments must be integers. " + usage)
+    # ensure two int args are w/in range
+    if not 1 <= threads <= 10 or not 10 <= points <= 1000000:
+        sys.exit("Arguments must be in range. " + usage)
+    return points, threads
+
 if __name__ =="__main__":
-    # TODO: validate inputs
-    threads = 2
-    points = 100000
+    # validate inputs
+    argv = sys.argv[1:]
+    points, threads = validateInputs(argv)
 
     # determine partitions
     partSize = int(points / threads)
@@ -39,6 +57,10 @@ if __name__ =="__main__":
     start = time.time()
     threadList = []
     for i in range(threads):
+        # ensure unevenly divided part gets given to last thread
+        if i == threads-1:
+            partSize += remainder
+        # create and start threads
         t = threading.Thread(target=countInRange, args=(partSize,))
         threadList.append(t)
         t.start()
@@ -51,6 +73,6 @@ if __name__ =="__main__":
     # output data
     estimate = 4 * float(globalCount) / points
     delta = abs(math.pi - estimate)
-    print("Time for computation; %f\n", dur)
-    print("Result: %f\n", estimate)
-    print("Delta: %f\n", delta)
+    print("Time for computation: %.6f seconds" % dur)
+    print("Result: %.4f" % estimate)
+    print("Delta: %.4f" % delta)
