40ef799cf0d8ed081b8acd8a3a1438cf8c7ed9c0
matthewgraca <mgraca@cpp.edu>
2022-11-09 20:10:28 -0800

    added lab05

---
+++ lab05/run_matrix_python.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=RUN_LOG
+#SBATCH --output=RUN_LOG_%j.txt
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:gp100gl:2
+#SBATCH --time=00:02:00
+#SBATCH --nodelist=cn02
+
+. /etc/profile.d/modules.sh
+
+module load cuda/10.2 python/3/pycuda/2019.1.1
+
+python3.4 matrix.py
